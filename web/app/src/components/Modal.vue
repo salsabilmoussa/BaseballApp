bc1783be02be2ebe03c0d6f91adc502dc6d680ea
salsabil <salsabilmoussa2001@gmail.com>
2025-02-21 11:49:50 +0100

    Gestion des cartes

---
+++ web/app/src/components/Modal.vue
@@ -0,0 +1,149 @@
+<template>
+    <div v-if="isOpen" class="modal">
+        <div class="modal-content">
+            <span class="close" @click="closeModal">&times;</span>
+            <h3>{{ title }}</h3>
+            <form @submit.prevent="submitForm">
+                <div class="form-group">
+                    <label for="name">Titre</label>
+                    <input type="text" v-model="formData.name" required>
+                </div>
+                <div class="form-group">
+                    <label for="player">Joueur</label>
+                    <input type="text"  v-model="formData.player" required>
+                </div>
+                <div class="form-group">
+                    <label for="team">Equipe</label>
+                    <input type="text" v-model="formData.team" required>
+                </div>
+                <div class="form-group">
+                    <label for="year">Année</label>
+                    <input type="number" v-model="formData.year" required>
+                </div>
+                <div class="form-group">
+                    <label for="imageUrl">URL de l'image</label>
+                    <input type="text" v-model="formData.imageUrl" required>
+                </div>
+                <div class="button-container">
+                    <button type="button" class="submit-btn" @click="closeModal">Annuler</button>
+                    <button type="submit" class="submit-btn">{{ submitButtonText }}</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</template>
+<script>
+
+export default ({
+    name: 'ModalComponent',
+    props:{
+        isOpen:{
+            type: Boolean,
+            required:true,
+        },
+        title:{
+            type: String,
+            required:true,
+        },
+        submitButtonText:{
+            type: String,
+            required:true,
+        },
+        initialData: {
+            type: Object,
+            default: () => ({
+                name: '',
+                player: '',
+                team: '',
+                year: '',
+                imageUrl: '',
+            }),
+        },
+    },
+    data() {
+        return {
+            formData: { ...this.initialData },
+        };
+    },
+    watch: {
+        initialData: {
+            handler(newData) {
+                this.formData = { ...newData };
+            },
+        },
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        },
+        submitForm() {
+            this.$emit('submit', this.formData);
+        },
+    },
+})
+</script>
+
+<style>
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    width: 400px;
+    position: relative;
+}
+
+.close {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 1.5rem;
+    cursor: pointer;
+}
+
+.form-group {
+    margin-bottom: 1rem;
+}
+
+.form-group label {
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+.form-group input {
+    width: 90%;
+    padding: 8px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+}
+
+.submit-btn {
+    padding: 10px 20px;
+    background-color: #3498db;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 1rem;
+    transition: background-color 0.3s ease;
+}
+.button-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+}
+
+</style>
--- web/app/src/views/CardsPage.vue
+++ web/app/src/views/CardsPage.vue
@@ -3,7 +3,53 @@
         <Header />
         <SideBar />
         <main class="main-content">
-            <h3>cards</h3>
+            <h3>Liste des cartes</h3>
+            <button @click="openAddCardModal"  class="add-card-btn">
+                <i class="fas fa-plus"></i> Ajouter</button>
+            <div class="search-container">
+            <i class="fas fa-search search-icon"></i>
+            <input type="text" v-model="search" placeholder="Rechercher une carte.." class="search-input">
+            </div>
+            <table class="cards-table">
+                <thead>
+                    <tr>
+                        <th>Titre</th>
+                        <th>Joueur</th>
+                        <th>Equipe</th>
+                        <th>Année</th>
+                        <th>Image</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="card in paginatedCards" :key="card.id">
+                        <td>{{ card.name }}</td>
+                        <td>{{ card.player }}</td>
+                        <td>{{ card.team }}</td>
+                        <td>{{ card.year }}</td>
+                        <td>
+                            <img :src="card.imageUrl" alt="Card Image" class="card-image">
+                        </td>
+                        <td>
+                            <button class="icon-btn edit-btn" @click="openEditCardModal(card)"><i class="fas fa-edit"></i></button>
+                            <button class="icon-btn delete-btn" @click="deleteCard(card.id)"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="pagination">
+                <button @click="prevPage" :disabled="currentPage == 1">Précédent</button>
+                <span>Page {{ currentPage }} sur {{ totalPages }}</span>
+                <button @click="nextPage" :disabled="currentPage == totalPages">Suivant</button>
+            </div>
+            <ModalComponent
+                :isOpen="isModalOpen"
+                :title="modalTitle"
+                :submitButtonText="modalSubmitButtonText"
+                :initialData="selectedCard"
+                @close="closeModal"
+                @submit="handleModalSubmit"
+            />
         </main>
     </div>
 </template>
@@ -12,20 +58,78 @@
 import Header from '@/components/Header.vue';
 import SideBar from '@/components/SideBar.vue';
 import { secondClient } from '@/api';
+import ModalComponent from '@/components/Modal.vue';
+import '@/assets/styles.css';
 
 export default {
     name: 'CardsPage',
     components: {
         Header,
-        SideBar
+        SideBar,
+        ModalComponent
     },
     data() {
         return {
             cards: [],
+            currentPage: 1,
+            itemsPerPage: 6,
+            isModalOpen: false,
+            modalTitle: '',
+            modalSubmitButtonText: '',
+            selectedCard: {
+                id: null,
+                name: '',
+                player: '',
+                team: '',
+                year: '',
+                imageUrl: '',
+            },
         };
     },
 
+    computed: {
+        paginatedCards() {
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            const end = start + this.itemsPerPage;
+            return this.cards.slice(start, end);
+        },
+        totalPages() {
+            return Math.ceil(this.cards.length / this.itemsPerPage);
+        },
+    },
+
     methods: {
+
+    openAddCardModal() {
+        this.modalTitle = 'Ajouter une carte';
+        this.modalSubmitButtonText = 'Ajouter';
+        this.selectedCard = { id: null, name: '', player: '', team: '', year: '', imageUrl: '' };
+        this.isModalOpen = true;
+    },
+
+    openEditCardModal(card) {
+        this.modalTitle = 'Modifier la carte';
+        this.modalSubmitButtonText = 'Modifier';
+        this.selectedCard = { ...card };
+        this.isModalOpen = true;
+    },
+
+    async handleModalSubmit(newData){
+        if(this.selectedCard.id){
+            await this.editCard(this.selectedCard.id, newData);
+        }
+        else{
+            await this.addCard(newData);
+        }
+        this.closeModal();
+    },
+
+
+    closeModal() {
+        this.isModalOpen = false;
+    },
+
+
     async getCards() {
         try {
                 const response = await secondClient.get('/cards');
@@ -33,6 +137,12 @@ export default {
             } catch (error) {
                 alert("Erreur lors de la récupération des cartes. Veuillez réessayer.");
             }
+        },
+    prevPage(){if (this.currentPage> 1)
+        this.currentPage --;
+    },
+    nextPage() {
+            if (this.currentPage < this.totalPages) this.currentPage++;
         },
 
     async addCard(cardData) {
@@ -67,10 +177,3 @@ mounted() {
     },
 }
 </script>
-<style scoped>
-.main-content {
-    flex: 1;
-    margin-right: 60%;
-    margin-top: 5%;
-}
-</style>
